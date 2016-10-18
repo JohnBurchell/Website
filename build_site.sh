12773e4fe5f9b0be9004c477e79dee1c287ff741
John Burchell <john.a.burchell@gmail.com>
2016-10-18 11:39:00 +0200

    Updated build script - post push hooks coming soon

--- build_site.sh
+++ build_site.sh
@@ -1,16 +1,28 @@
-#Auto build and move sites to their respective locations
+#!/bin/bash
+# Auto build and move sites to their respective locations
 
-git pull origin master
+# Check command options
+while [ "$1" != "" ]; do
+  case $1 in
+    -git | -g )
+      echo "Updating from Git.."
+      #Move and push into the corect folder.
+      git pull origin master
+      #Portfolio content - This should essentially mirror the content on the git repo
+      echo "Copying static portfolio content.."
+      sudo cp content/* /var/www/html/johnburchell.co.uk/public_html
+  esac
+  shift
+done
 
-#Portfolio content - This should essentially mirror the content on the git repo
-#Move and push into the corect folder.
-
-sudo cp content/* /var/www/html/johnburchell.co.uk/public_html
-
-#Programming blog
+# Always build the blog pages
+# Programming blog
 cd ~/website/programming_blog
+echo "Entering: " $(pwd)
+echo "Building Warhammer blog"
 sudo jekyll build --destination /var/www/html/programming.johnburchell.co.uk/public_html
-
-#Warhammer blog
-cd ~/webiste/warhammer_blog
+cd ~/website/warhammer_blog
+echo "Entering: " $(pwd)
+echo "Building Programming blog"
+# Warhammer blog
 sudo jekyll build --destination /var/www/html/warhammer.johnburchell.co.uk/public_html
